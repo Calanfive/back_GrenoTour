7e24c52b3a42fb6969f4a7f9e2e5dc5a842f6856
KarimSekri <karim.sekri@gmail.com>
2024-01-17 11:39:43 +0100

    Ajout register et  connexion ok

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,9 +1,12 @@
 import { DataTypes, Sequelize } from "sequelize";
 
 export const UserModel  = (sequelize: Sequelize) => {
-    return sequelize.define('user', {
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        email: DataTypes.STRING
-    });
+    return sequelize.define('utilisateurs', {
+        favoris_id: {type: DataTypes.INTEGER},
+        nom: {type: DataTypes.STRING},
+        prenom:{type: DataTypes.STRING},
+        mail: {type: DataTypes.STRING},
+        mdp: {type: DataTypes.STRING},
+        telephone:{type: DataTypes.INTEGER},
+    }, {timestamps: false});
 }
