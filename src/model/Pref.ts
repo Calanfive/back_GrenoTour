2ed2288e61cd790e9301291f672e0796ca67cf5e
calanfive <camille.lelan@gmail.com>
2024-01-18 12:18:30 +0100

    12_CLL_Model_Passage

--- src/model/Pref.ts
+++ src/model/Pref.ts
@@ -1,6 +1,6 @@
 import { DataTypes, Sequelize } from "sequelize";
 
-export const Pref= (sequelize: Sequelize) => {
+export const PrefModel= (sequelize: Sequelize) => {
     return sequelize.define('pref',{
         langue: DataTypes.STRING
     })
