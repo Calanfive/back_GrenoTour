05c93f44b4fe4ee899a61c8f2176efe499072aa4
calanfive <camille.lelan@gmail.com>
2024-01-19 14:58:41 +0100

    16_CLL_Associations_correctifs

--- src/model/Favoris.ts
+++
@@ -1,8 +0,0 @@
-import { DataTypes, Sequelize } from "sequelize";
-
-export const FavorisModel= (sequelize: Sequelize) => {
-    return sequelize.define('favoris',{
-        utilisateur_id: DataTypes.INTEGER,
-        itineraire_id: DataTypes.INTEGER,
-    }, {timestamps: false});
-}
--- src/model/Point_de_passage.ts
+++
@@ -1,8 +0,0 @@
-import { DataTypes, Sequelize } from "sequelize";
-
-export const PointDePassageModel= (sequelize: Sequelize) => {
-    return sequelize.define('pointdepassage',{
-        lieu_id: DataTypes.STRING,
-        itineraire_id: DataTypes.STRING,
-    }, {timestamps: false});
-}
