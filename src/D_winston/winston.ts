b76c86bdf15f839935a25c715a9b3e0c59db0882
aymeric <rambaudaymeric@gmail.com>
2024-01-19 15:17:22 +0100

    problème de mise en place des loggers

--- src/D_winston/winston.ts
+++ src/D_winston/winston.ts
@@ -1,20 +1,20 @@
-export function winston_log() {
-  const winston = require('winston');
-  
-  const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-      new winston.transports.File({ filename: 'D_winston_log/combined.log' }),
-      new winston.transports.File({ filename: 'D_winston_log/error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'D_winston_log/debug.log', level: 'debug' }),
-    ],
-  });
+import {createLogger, format, transports } from "winston";
+import winston from "winston";
 
-  // if (process.env.NODE_ENV !== 'production') {
-  //   logger.add(new winston.transports.Console({
-  //     format: winston.format.simple(),
-  //   }));
-  // }
-}
+const { combine, timestamp, label, prettyPrint } = format;
+
+export const logger = winston.createLogger({
+  level: 'info',
+  defaultMeta: { service: 'user-service' },
+  format: format.combine(
+    format.json(),
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'D_winston_log/combined.log' }),
+    new winston.transports.File({ filename: 'D_winston_log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'D_winston_log/http.log', level: 'http' }),
+    new winston.transports.File({ filename: 'D_winston_log/debug.log', level: 'debug' }),
+  ],
+})
