720a9db1084ab0b02ac75672305ab040fe327a95
KarimDev <karim.sekri@gmail.com>
2024-03-12 13:36:17 +0100

    Ajout de l'interface DecodeToken

--- src/router/route.user.ts
+++ src/router/route.user.ts
@@ -4,18 +4,9 @@ import "dotenv/config";
 
 export const userRouter = Router();
 
-userRouter.get("/findUser", async(req, res) => {
+userRouter.get("/findUserMe", async(req, res) => {
     const alluser = await User.findAll();
     res.status(200).send(alluser.map(User => User.dataValues))
 })
 
-userRouter.post("/register", async(req, res) => {
-    const {pseudo, mail, mdp, telephone } = req.body;
-    if ( !pseudo || !mail || !mdp || !telephone) {
-        res.status(400).send("Information manquante")
-    }
-    else {
-        const userToRegister = await User.create();
-        res.status(200).send(userToRegister)
-    }
-})
+//put (update)
